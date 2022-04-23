ec8d00c9b92971357b1509c27cbef141c7e1533a
Patrick Cavanagh <pcavanagh@discovernewfields.org>
2022-04-23 12:03:02 -0400

    updated cognito config values

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_eVKBMypKU', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'r38kte7529vopcsl9bpg0e6kk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
